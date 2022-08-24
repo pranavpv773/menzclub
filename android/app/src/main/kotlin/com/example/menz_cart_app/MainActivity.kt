e7752cddb743626a499e47bcd854997007cd8aa7
pranavpv773 <pranavpv773@gmail.com>
2022-08-24 16:44:43 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/menz_cart_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.menz_cart_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:menz_cart_app/sign_up/view/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
