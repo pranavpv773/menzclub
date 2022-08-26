c433856be38d76f5a8aee45dab4d87256942af08
pranavpv773 <pranavpv773@gmail.com>
2022-08-26 19:38:21 +0530

    home screen updation

---
+++ lib/home/view/widgets/third_banner.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ThirdBanner extends StatelessWidget {
+  const ThirdBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 500,
+      height: 300,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            'https://scontent-bom1-1.xx.fbcdn.net/v/t1.6435-9/39132368_10156660247218221_880685569485570048_n.jpg?stp=dst-jpg_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=a5Do2QIJHMkAX99Q7QM&_nc_ht=scontent-bom1-1.xx&oh=00_AT-EO-AeNI4u_HUZ5oQpZixb77JweMnlkX1mM9TG-menbQ&oe=632C7FEE',
+          ),
+        ),
+      ),
+    );
+  }
+}
