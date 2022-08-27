3ce444b24e577f3d18e7e292488961f142ea4e91
pranavpv773 <pranavpv773@gmail.com>
2022-08-27 16:23:05 +0530

    shirt screen Color section added

--- lib/shirt/view/shirt_screen.dart
+++ lib/shirt/view/shirt_screen.dart
@@ -3,7 +3,13 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:menz_cart_app/constants/colors.dart';
 import 'package:menz_cart_app/home/view/widgets/carousel_banner.dart';
 import 'package:menz_cart_app/home/view/widgets/circle_dot.dart';
+import 'package:menz_cart_app/home/view/widgets/grid_view_card.dart';
 import 'package:menz_cart_app/home/view_model/shirts.dart';
+import 'package:menz_cart_app/shirt/view/widgets/shirt_fit.dart';
+import 'package:menz_cart_app/shirt/view_model/map_shirt.dart';
+
+import 'widgets/shirt_category.dart';
+import 'widgets/top_collecton.dart';
 
 class ShirtScreen extends StatelessWidget {
   const ShirtScreen({Key? key, required this.string}) : super(key: key);
@@ -14,121 +20,52 @@ class ShirtScreen extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        body: SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Column(
-        children: [
-          HomeCarouselBanners(
-            width: width,
-            heights: height / 2,
-            list: shirts,
-          ),
-          const DotIndicator(),
-          ShirtCategoryCards(
-            width: width,
-            height: height,
-          )
-        ],
-      ),
-    ));
-  }
-}
-
-class ShirtCategoryCards extends StatelessWidget {
-  const ShirtCategoryCards({
-    Key? key,
-    required this.width,
-    required this.height,
-  }) : super(key: key);
-
-  final double width;
-  final double height;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        ShirtCategories(
-          width: width,
-          height: height,
-          image:
-              'https://getketchadmin.getketch.com/product/8905040617459/660/HLSH011581_1.JPG',
-          caption: 'CASUAL',
-        ),
-        ShirtCategories(
-          width: width,
-          height: height,
-          image:
-              'https://5.imimg.com/data5/IN/BF/MY-5837050/men-formal-shirt-1000x1000.jpg',
-          caption: 'FORMAL',
-        ),
-        ShirtCategories(
-          width: width,
-          height: height,
-          image:
-              'https://rukminim1.flixcart.com/image/612/612/kpodocw0/shirt/s/q/u/l-ronit-ap001-pushya-original-imag3uwarrta9bdy.jpeg?q=70',
-          caption: 'PARTY',
-        ),
-      ],
-    );
-  }
-}
-
-class ShirtCategories extends StatelessWidget {
-  const ShirtCategories({
-    Key? key,
-    required this.width,
-    required this.height,
-    required this.image,
-    required this.caption,
-  }) : super(key: key);
-
-  final double width;
-  final double height;
-  final String image;
-  final String caption;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ContainerImage(
-          width: width,
-          height: height,
-          image: image,
-        ),
-        Text(
-          caption,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontFamily: GoogleFonts.abhayaLibre().fontFamily,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class ContainerImage extends StatelessWidget {
-  const ContainerImage({
-    Key? key,
-    required this.width,
-    required this.height,
-    required this.image,
-  }) : super(key: key);
-
-  final double width;
-  final double height;
-  final String image;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width / 4,
-      height: height / 6,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(image),
-          fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                HomeCarouselBanners(
+                  width: width,
+                  heights: height / 2,
+                  list: shirts,
+                ),
+                const DotIndicator(),
+                ShirtCategoryCards(
+                  width: width,
+                  height: height,
+                ),
+                TopCollectionsWidget(
+                  height: height,
+                  width: width,
+                ),
+                ShirtFitWidget(
+                  height: height,
+                  width: width,
+                ),
+                Column(
+                  children: [
+                    ShirtContentBanner(
+                      width: width,
+                      height: height,
+                      image: 'assets/banners/shirt3.jpg',
+                      topic: 'SHOP  BY COLOR',
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                    ),
+                    ShirtBannerBuilder(
+                      width: width,
+                      height: height,
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const ShopNowButton(
+              textButton: 'SHOP ALL SHIRTS',
+            )
+          ],
         ),
       ),
     );
