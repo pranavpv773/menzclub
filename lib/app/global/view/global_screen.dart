8b4d9819341942ff8734d64ff48c082eab2ed882
pranavpv773 <pranavpv773@gmail.com>
2022-10-17 20:04:48 +0530

    silver app bar added

--- lib/app/global/view/global_screen.dart
+++ lib/app/global/view/global_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:menz_cart_app/app/app_style/color_style.dart';
 import 'package:menz_cart_app/app/global/view_model/global_provider.dart';
 import 'package:provider/provider.dart';
 import 'widgets/appbar_widget.dart';
@@ -13,18 +14,45 @@ class GlobalScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: context.watch<GlobalProvider>().showAppbar == true
-            ? const PreferredSize(
-                preferredSize: Size.fromHeight(120),
-                child: ActiveApppbarWidget(),
-              )
-            : const PreferredSize(
-                preferredSize: Size.fromHeight(120),
-                child: SizedBox(),
-              ),
-        body: Consumer<GlobalProvider>(builder: (context, value, _) {
-          return value.pages[value.pageIndex];
-        }),
+        // appBar: context.watch<GlobalProvider>().showAppbar == true
+        //     ? const PreferredSize(
+        //         preferredSize: Size.fromHeight(120),
+        //         child: ActiveApppbarWidget(),
+        //       )
+        //     : const PreferredSize(
+        //         preferredSize: Size.fromHeight(120),
+        //         child: SizedBox(),
+        //       ),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              context.watch<GlobalProvider>().showAppbar == false
+                  ? const SliverAppBar(
+                      toolbarHeight: 0,
+                    )
+                  : SliverAppBar(
+                      backgroundColor: AppColor.primary1,
+                      expandedHeight: 120.0,
+                      floating: false,
+                      pinned: false,
+                      snap: false,
+                      flexibleSpace:
+                          context.watch<GlobalProvider>().showAppbar == true
+                              ? const PreferredSize(
+                                  preferredSize: Size.fromHeight(120),
+                                  child: ActiveApppbarWidget(),
+                                )
+                              : const PreferredSize(
+                                  preferredSize: Size.fromHeight(0),
+                                  child: SizedBox(),
+                                ),
+                    )
+            ];
+          },
+          body: Consumer<GlobalProvider>(builder: (context, value, _) {
+            return value.pages[value.pageIndex];
+          }),
+        ),
         bottomNavigationBar: const BottomNavyWidget(),
       ),
     );
